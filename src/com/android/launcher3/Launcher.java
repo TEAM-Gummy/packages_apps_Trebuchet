49290445e0f5bf1c302ea22984169a244b2bff1b
Clark Scheff <clark@cyngn.com>
2014-08-25 11:28:03 -0700

    Revert "Handle theme changes"
    This reverts commit 64718d775ce8322915d143adb9a0f1ea3160e9e3.
    
    Change-Id: Ibf92232dfa8ee96aafecc4b4e49fc55cfb21c229

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -592,7 +592,7 @@ public class Launcher extends Activity
                 R.bool.preferences_interface_homescreen_search_screen_left_default);
     }
 
-    void initializeDynamicGrid() {
+    private void initializeDynamicGrid() {
         LauncherAppState.setApplicationContext(getApplicationContext());
         LauncherAppState app = LauncherAppState.getInstance();
 
